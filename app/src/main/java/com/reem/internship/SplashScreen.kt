621047438ea68cbb76c0b6a2a7f8256fed9b2e73
Reem Altammami <reem.saud.ruh.k@tuwaiq.edu.sa>
2022-01-18 02:25:44 +0300

    set dark mode

--- app/src/main/java/com/reem/internship/SplashScreen.kt
+++ app/src/main/java/com/reem/internship/SplashScreen.kt
@@ -4,10 +4,16 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import androidx.appcompat.app.AppCompatDelegate
 import com.reem.internship.auth.SignUpActivity
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES) {
+            setTheme(R.style.Theme_Internship)
+        } else {
+            setTheme(R.style.Theme_Internship)
+        }
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
